df1f5860f95b3332fb8364731c5c6adf19f44f32
Dan McElroy <dan.mcelroy@huuugegames.com>
2020-05-21 15:50:10 +0100

    Initial commit.

---
+++ WebApplication/Program.cs
@@ -0,0 +1,22 @@
+using ElectronNET.API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseElectron(args);
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
